e6c13e02a18af06ba24b4b0b87e06dff9944b0df
Joel Ricci-Lopez <ricci.lob@gmail.com>
2019-07-24 21:13:58 -0600

    First version, interfase added

---
+++ particles.py
@@ -0,0 +1,56 @@
+import numpy as np
+from abc import ABC, abstractmethod
+
+# Interfase
+class ParticlesGenerator(ABC):
+    @abstractmethod
+    def generate_initial_state():
+        pass
+
+class RandomGenerator(ParticlesGenerator):
+    def __init__(self, num_particles, box_length):
+        # assuming the object Box has an atribute or a
+        self.num_particles = num_particles
+        self.box_length = box_length
+        self.coordinates = self.generate_initial_state()
+
+    def generate_initial_state(self):
+        coordinates = (0.5 - np.random.rand(self.num_particles)) * self.box_length
+        return coordinates
+
+class FileGenerator(ParticlesGenerator):
+    def __init__(self, file_name):
+        # assuming the object Box has an atribute or a
+        self.coordinates = self.generate_initial_state()
+        self.num_particles = len(coordinates)
+        self.box_length
+
+    @property
+    def box_length(self):
+        length = self.coordinates.max() - self.coordinates.min()
+        return length
+
+    
+    def generate_initial_state(self):
+        coordinates = np.loadtxt(self.file_name, skiprows=2, usecols=(1, 2, 3))
+        return coordinates
+''' 
+Brian
+    def get_box_length(self):
+        box_length = 
+'''
+
+class Particles:
+    def __init__(self, generate_method):
+        self.generate_method = generate_method
+        self.coordinates
+
+    @property
+    def coordinates(self, **kwargs):
+        if self.generate_method == 'random':
+            return RandomGenerator(**kwargs).generate_initial_state()
+        else:
+            # rise error if file doesn't exist
+            return FileGenerator(**kwargs).generate_initial_state()
+
+
