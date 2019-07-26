54f35f47269836abdbe172c4d0050674c652665b
VAN NGUYEN <vtnguye4@ncsu.edu>
2019-07-26 14:57:25 -0500

    Create potential method

--- Pairwise_potential.py
+++ Pairwise_potential.py
@@ -8,9 +8,21 @@ class PairwisePotential(ABC):
 
     rij2 = square distance between two particles
 
+    Return
+    ------
+
+    Pairwise potential energy from suitable potential:
+        + Lennard-Jones
+        + Hard-sphere
+        + Square-well
+
     """
     @abstractmethod
-    def __call__(self,rij2):
+    def potential(self, rij2):
+        pass
+
+    @abstractmethod
+    def __call__(self, rij2):
         pass
 
 class LJ(PairwisePotential):
@@ -27,32 +39,20 @@ class LJ(PairwisePotential):
 
     """
     
-    def __init__(self,sigma=1.0,epsilon=1.0, cutoff=2.6):
+    def __init__(self, sigma=1.0, epsilon=1.0, cutoff=2.6):
 
         self.sigma = sigma
         self.epsilon = epsilon
+        self._cutoff = cutoff
         self.cutoff2 = cutoff * cutoff
     
-    def lennard_jones(self, rij2):
-        """Pairwise potential energy by Lennard-Jones potential
+    def potential(self, rij2):
 
-    Parameters
-    ----------
-
-    rij2 : float
-        Square distance between two particles
-
-    Return
-    ------
-
-    Pairwise Lennard-Jones potential energy at considered distance
-    
-    """
         sig_by_r6 = np.power(self.sigma/rij2,3)
         sig_by_r12 = np.power(sig_by_r6,2)
         return 4.0*self.epsilon*(sig_by_r12-sig_by_r6)
 
-    def cutoff_correction(self, cutoff2, box_object, num_particles,):
+    def cutoff_correction(self, box_object, num_particles,):
         """The function corrects interaction energy from energy cutoff.
 
     Parameters
@@ -75,16 +75,16 @@ class LJ(PairwisePotential):
     """
 
         volume = box_object.volume
-        sig_by_cutoff3 = np.power(self.sigma/cutoff2, 3)
+        sig_by_cutoff3 = np.power(self.sigma/self._cutoff, 3)
         sig_by_cutoff9 = np.power(sig_by_cutoff3, 3)
         e_correction = sig_by_cutoff9 - 3.0 * sig_by_cutoff3
-        e_correction *= 8.0 / 9.0 * np.pi * num_particles / volume * num_particles
+        e_correction *= 8.0 / 9.0 * np.pi * np.power(num_particles,2) * self.epsilon * np.power(self.sigma,3)/ volume
         return e_correction
 
     def __call__(self, rij2):
 
         if rij2 < self.cutoff2:
-            e_pair = lennard_jones(rij2)
+            e_pair = self.potential(rij2)
         else:
             e_pair = 0.0
         return e_pair
@@ -99,7 +99,10 @@ class HS(PairwisePotential):
 
     """
 
-    def __call__(self,rij2):
+    def potential(self, rij2):
+        pass
+
+    def __call__(self, rij2):
         pass
 
 class SW(PairwisePotential):
@@ -112,7 +115,10 @@ class SW(PairwisePotential):
 
     """
 
-    def __call__(self,rij2):
+    def potential(self, rij2):
+        pass
+
+    def __call__(self, rij2):
         pass
 
 
