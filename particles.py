c5f8ea79064619c74b49fbe349978cdf17646814
Joel Ricci-Lopez <ricci.lob@gmail.com>
2019-07-25 22:29:48 -0600

    Added particles class with implemented subclasses

--- particles.py
+++ particles.py
@@ -1,112 +1,149 @@
+"""
+particles.py
+A python package for the MolSSI Software Summer School.
+Contains Particles class and subclasses RandomParticles and FetchParticles
+"""
+
 import numpy as np
+from box import *
 from abc import ABC, abstractmethod
 
-class ParticlesGenerator(ABC):
-    ''' Interface ParticlesGenerator to obtain a Particles Object
+class Particles(ABC):
+    ''' Abstract Particles class
     '''
+
     def __init__(self):
-        ''' RandomGenerator Class Constructor.
-
-        Parameters:
-        -----------
-            num_particles (int) : Number of particles of the system to evaluate. 
-            box (numpy.array) : An array of shape (3,) with the dimensions of the box (x, y, z). 
+        '''The constructor of the Particles Class.
         '''
-        self._num_particles = num_particles
-        self._box = box #  TODO: Implemented in box should return an array of 3 dimensions
-        self._coordinates = coordinates
+        self._num_particles = 0
+        self._coordinates = []
 
-    @property  # getter
-    def coordinates(self):
-        return self._particles
-
-    @coordinates.setter #setter
-    def num_particles(self):
-        self._num_particles = len(self._coordinates)
-
-    @property  # getter
-    def coordinates(self):
-        return self._particles
+    def __str__(self):
+        return( F'Particles Object: {self._num_particles} particles.' )
 
     @abstractmethod
-    def coordinates(self):
+    def _generate_coordinates(self):
+        '''Generates the coordinates of the Particles Object '''
         pass
-'''
-    @abstractmethod
-    def generate_initial_state():
-        pass'''
 
-class RandomParticles(ParticlesGenerator):
-    ''' This class returns a Particles Object with random positions given number of particles an box dimensions. 
-    '''
-    def __init__(self, num_particles, box):
-        ''' RandomGenerator Class Constructor.
-
-        Parameters:
-        -----------
-            num_particles (int) : Number of particles of the system to evaluate. 
-            box (numpy.array) : An array of shape (3,) with the dimensions of the box (x, y, z). 
-        '''
-    
-    @staticmethod
+    @property
     def coordinates(self):
-        coordinates = (0.5 - np.random.rand(self._num_particles, 3)) * self._box[np.newaxis,:]
-        self._coordinates = coordinates
-
-
-class FileParticles(ParticlesGenerator):
-    def __init__(self, file_name):
-        self.file_name = file_name
-        # self.box = get_box_length() # TODO: Implemented in box
-
-    ''' @Brian
-    def get_box_length(self):
-        length = self.coordinates.max() - self.coordinates.min()
-        return length
-        '''
-    
-    def generate_initial_state(self):
-        coordinates = np.loadtxt(self.file_name, skiprows=2, usecols=(1, 2, 3))
-        return Particles(coordinates)
-
-
-class Particles:
-    def __init__(self, coordinates):
-        self._coordinates = coordinates
-       # self._num_particles = num_particles
-
-    @property  # getter
-    def coordinates(self):
+        '''Returns the coordinates of the Particles Object'''
         return self._coordinates
 
-    @coordinates.setter #setter
+    @property
     def num_particles(self):
-        self._num_particles = len(self._coordinates)
-
-    @property  # getter
-    def num_particles(self):
+        '''Returns the coordinates of the Particles Object'''
         return self._num_particles
 
-    ''' We really need the user pass coordinates directly to th object?
-    @coordinates.setter  # setter
-    def coordinates(self, new_coordinates):
-        self._coordinates = new_coordinates
-        self.bonds = self.build_bond_list()
+    @property
+    def box(self):
+        '''Returns the Box Object of the Particles Object'''
+        return self._box
+    
+    def get_num_particles(self):
+        '''Returns the number of particles of the Particles Object'''
+        self._num_particles = len(self._coordinates)
+
+
+class RandomParticles(Particles):
+    ''' This class returns a Particles Object with random positions given number of particles an box dimensions. 
+    '''
+    def __init__(self, num_particles, box_dims):
+        ''' The constructor of the RandomParticles Class.
+
+        Parameters:
+        -----------
+            num_particles : int
+                Number of particles of the system to evaluate. 
+            box_dims : numpy.array
+                An array of shape (3,) with the dimensions of the box (x, y, z).
         '''
+        super().__init__()
+        self._num_particles = num_particles
+        self._box = Box(box_dims)
+        self._coordinates = self._generate_coordinates()
+    
+    def _generate_coordinates(self):
+        '''Generates the coordinates of the RandomParticles Object 
 
+            Returns
+            -------
+            coordinates : np.array
+                Arrays of the wrapped atomic coordinates.
+        '''
+        coordinates = (0.5 - np.random.rand(self.num_particles, 3)) * self._box.box_dim[np.newaxis,:]
+        return coordinates
+
+
+class FetchParticles(Particles):
+    ''' This class returns a Particles Object with random positions given number of particles an box dimensions. 
+    '''
+
+    def __init__(self, file_name, wrap_dims = None): #TODO: skiprows=2, usecols=(1, 2, 3)
+        ''' FileParticles Class Constructor.
+
+        Parameters:
+        -----------
+            file_name : str
+                A string with the path to the file with x, y, z coordinates.
+            wrap_dims : None, np.array
+                Determines wheter wraps the particles within the box dimensions (np.array, shape=(3,)) 
+                or takes the length dimensions from the file (None). Default None.
+        '''
+        super().__init__()
+        self._file_name = file_name
+        self._wrap_dims = wrap_dims
+        self._coordinates = self._generate_coordinates()
+        self._num_particles = self.get_num_particles()
+        self._box = self._create_box()
+        
+    def _generate_coordinates(self):
+        '''Generates the coordinates of the FetchParticles Object 
+
+            Returns
+            -------
+            coordinates : np.array
+                Arrays of the wrapped atomic coordinates.'''
+        coordinates = np.loadtxt(self._file_name, skiprows=2, usecols=(1, 2, 3))
+        return coordinates
+
+    def get_num_particles(self):
+        return (len(self.coordinates))
+
+    def _create_box(self):
+        '''Generates the Box object the FetchParticles Object 
+        
+            Returns
+            -------
+            box : Box class object
+                An Box class object.'''
+        if self._wrap_dims is None:
+            box_dims = abs( self.coordinates.max(axis=0) - self.coordinates.min(axis=0) )
+            box = Box(box_dims)
+
+        elif isinstance(self._wrap_dims, np.ndarray):
+            box_dims = self._wrap_dims
+            box = Box(box_dims)
+            self._coordinates = box.wrap(self._coordinates)
+
+        return box
+
+
+# Uncoment to test
 '''
-    #@property
-    def coordinates(self, **kwargs):
-        if self.generate_method == 'random':
-            return RandomGenerator(self, **kwargs).generate_initial_state()
-        else:
-            # rise error if file doesn't exist
-            return FileGenerator(self, **kwargs).generate_initial_state()
+num_particles = 3
+box_dims = np.array([15, 15, 15])
+part =  RandomParticles(num_particles, box_dims)
+#part = FetchParticles('./sample_config1.xyz', wrap_dims = box_dims)
+
+# Creating a Particles Object
+print( part.coordinates)
+print(F'\nNum particles: {part.num_particles} \nBox dims: {part.box.box_dim}')
+# Rewraping the system example
+part.box.box_dim = np.array([1, 1, 1])
+part.box.wrap(part.coordinates)
+print(part.coordinates)
+print(F'\nNum particles: {part.num_particles} \nBox dims: {part.box.box_dim}')
+print(part)
 '''
-
-num = 10
-box = np.array([10, 10, 10])
-part =  RandomGenerator(num, box)
-print( part.coordinates )
-
-#print( FileGenerator('./sample_config1.xyz'))
