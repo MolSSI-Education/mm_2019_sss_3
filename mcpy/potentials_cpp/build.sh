1543202b102440c83b33b22bb1d0addad1675e41
Joel Ricci-Lopez <ricci.lob@gmail.com>
2019-07-30 14:38:07 -0600

    First atempt to implement LJ and cutoff_corre in c++

---
+++ mcpy/potentials_cpp/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+g++ -fPIC --shared -std=c++11 -Wall potentials.cpp export.cpp \
+-I${CONDA_PREFIX}/include \
+-I${CONDA_PREFIX}/include/python3.6m \
+-I${CONDA_PREFIX}/include/eigen3 \
+-L${CONDA_PREFIX}/lib -lpython3.6m \
+-o potentials_cpp.so
--- mcpy/potentials_cpp/potentials.hpp
+++ mcpy/potentials_cpp/potentials.hpp
@@ -4,9 +4,10 @@
 #include <Eigen/Dense>
 #include <vector>
 #include <math.h>
+#include <thread>
 
 
-double LJ(std::vector<double> coord_ij2, 
+double LJ(std::vector<double> & coord_ij2, 
             double sigma = 1.0, 
             double epsilon = 1.0,
             double cutoff2 = 2.6);
